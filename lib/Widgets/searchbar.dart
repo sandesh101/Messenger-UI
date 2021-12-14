f9a71e89f799a76b7d5a56bd2b02fac341f142ed
Sandesh Rimal <rimal.sandesh11@gmail.com>
2021-12-14 18:45:10 +0545

    Fix: Searchbar

--- lib/Widgets/searchbar.dart
+++ lib/Widgets/searchbar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:cupertino_icons/cupertino_icons.dart';
 
 class SearchBar extends StatelessWidget {
   @override
