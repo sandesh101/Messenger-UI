975c415e528c89d03bc9aabe2daf7923c9a6b8b2
Sandesh Rimal <rimal.sandesh11@gmail.com>
2021-12-16 15:48:48 +0545

    Added: Messages

--- lib/Widgets/mainChats.dart
+++ lib/Widgets/mainChats.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/material.dart';
 
 class MainChats extends StatelessWidget {
+  Widget peopleChat(String image, String name, String message) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 10.0),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 30,
+            backgroundImage: AssetImage(image),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                name,
+                style: TextStyle(fontSize: 20),
+              ),
+              Text(
+                message,
+                style: TextStyle(fontSize: 15),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 180.0),
-      child: Container(
-        child: Text('Main chat body'),
+      padding: const EdgeInsets.only(top: 170.0),
+      child: ListView(
+        children: [
+          peopleChat('assets/images/Suman.jpg', 'Suman Ghimire', 'Hus'),
+          peopleChat('assets/images/Suman.jpg', 'Suman Ghimire', 'Hus'),
+          peopleChat('assets/images/Suman.jpg', 'Suman Ghimire', 'Hus'),
+          peopleChat('assets/images/Suman.jpg', 'Suman Ghimire', 'Hus'),
+          peopleChat('assets/images/Suman.jpg', 'Suman Ghimire', 'Hus'),
+          peopleChat('assets/images/Suman.jpg', 'Suman Ghimire', 'Hus'),
+          peopleChat('assets/images/Suman.jpg', 'Suman Ghimire', 'Hus'),
+          peopleChat('assets/images/Suman.jpg', 'Suman Ghimire', 'Hus'),
+        ],
       ),
     );
   }
