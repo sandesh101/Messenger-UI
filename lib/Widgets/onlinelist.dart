928bb9b5d3c9c938546cac9d2e70a14433469b46
Sandesh Rimal <rimal.sandesh11@gmail.com>
2021-12-16 13:24:38 +0545

    Made: Online List field

--- lib/Widgets/onlinelist.dart
+++ lib/Widgets/onlinelist.dart
@@ -1,24 +1,47 @@
 import 'package:flutter/material.dart';
 
 class OnlineList extends StatelessWidget {
+  Widget onlinePerson(String imageLocation, String name, String lastName) {
+    return Column(
+      children: [
+        CircleAvatar(
+          backgroundImage: AssetImage(imageLocation),
+          radius: 30,
+        ),
+        Text(name),
+        Text(lastName),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(8.0, 60.0, 10, 20),
-      child: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10.0, 70.0, 20.0, 0.0),
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Column(
               children: [
                 IconButton(
                   onPressed: () {},
-                  icon: Icon(Icons.video_call),
+                  icon: const Icon(Icons.video_call),
                   iconSize: 30,
                 ),
-                Text('Make video call')
+                const Text('Make video'),
+                const Text('call'),
               ],
             ),
+            onlinePerson('assets/images/Ashish.jpg', 'Ashish', 'Mhrzn'),
+            onlinePerson('assets/images/Ashwina.jpg', 'Ashwina', 'Poudel'),
+            onlinePerson('assets/images/Muna.jpg', 'Ghising', 'Muna'),
+            onlinePerson('assets/images/Neupal.jpg', 'Neupal', 'Rai'),
+            onlinePerson('assets/images/Sanjeet.jpg', 'Sanjeet', 'Ghimire'),
+            onlinePerson('assets/images/Sanjog.jpg', 'Sanjog', 'Ghimire'),
+            onlinePerson('assets/images/Suman.jpg', 'Suman', 'Ghimire'),
+            onlinePerson('assets/images/Saroj.jpg', 'Saroj', 'Rimal'),
           ],
         ),
       ),
